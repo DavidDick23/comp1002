7906a4cec31f9fe02c7e8a4c6695a3f52545f909
David Dick <david.dck@gmail.com>
2025-11-28 17:40:53 -0500

    Added comments to styles.css and completed steps in current-page.nam.js

--- lab-e/js/current-page-nav.js
+++ lab-e/js/current-page-nav.js
@@ -1,15 +1,22 @@
 // STEP C: Get the current URL for the page
+const currentPage = window.location.href;
 
 // STEP D: Grab all the top-level <a> elements inside the <nav> element at the top of the page (inside the <header> element)
+const navLinks = document.querySelectorAll("header nav > ul > li > a");
 
 // STEP E: Create a place to store the href value for each <a> element
+let hrefValue = "";
 
 // STEP F: For each <a> element in the NodeList navLinks, compare the href property with the URL for the current page
-
+for (let i = 0; i < navLinks.length; i++)
+{
     // STEP G: Grab the href property of each <a> (element)
-    
+    hrefValue = navLinks[i].href; 
+
     // STEP H: Check for a match with the current page
-    
+    if (currentPage.includes(hrefValue)) 
+    {
         // STEP I: They match - so this <a> is the link for the current page
-        
-    
+        navLinks[i].classList.add("current");
+    }    
+}
